41754b43c061968f1b4c970854a54044117bff70
nathan <nathanshulkin@gmail.com>
2021-04-16 21:15:12 +0000

    working on scaffolding i think

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'bootstrap', '~> 5.0.0.beta2'
+
+
 group :production do
   gem 'pg', '~> 0.21' # for Heroku deployment
   gem 'rails_12factor'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'pages#main'
+
+  resources :posts
+
+  get 'welcome/index'
+  root to: 'welcome#index'
+
+  get 'post/new'
+  root to: 'post#new'
+
+  #root to: 'pages#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
