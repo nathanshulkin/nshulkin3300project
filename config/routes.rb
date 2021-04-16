45e74ba90919e7b4caaadcec5d78110530d1751a
nathan <nathanshulkin@gmail.com>
2021-04-16 21:54:53 +0000

    we can make posts now so thats neat

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
 
   resources :posts
 
-  get 'welcome/index'
-  root to: 'welcome#index'
-
   get 'posts/new'
   root to: 'posts#new'
 
+  # posts GET    /posts(.:format)          # postsindex
   #root to: 'pages#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
