50d0bd916bba0e17a8481e2e2331ad371afcd9ed
nathan <nathanshulkin@gmail.com>
2021-04-20 03:01:33 +0000

    edit and delete are back baby

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,23 @@ class PostsController < ApplicationController
     def create
         @post = Post.new(params[:post].permit(:title, :text))
  
-        @post.save
-        redirect_to @post
+        if @post.save
+            redirect_to @post
+        else
+            render "edit"
+        end
     end
- 
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_url, notice: "post succesfully eliminated"
+    end
+
     def show
         @post = Post.find(params[:id])
     end
