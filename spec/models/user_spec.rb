a2d502c193910b8875cb07b99571ba672f92783a
nathan <nathanshulkin@gmail.com>
2021-04-29 18:28:58 +0000

    taking out authentication for the moment because tests are failing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  expect(true).to eq(true)
+  context "validations users" do
+    it "ensures users signed in" do
+      project = Post.new(description: "no user signed in")
+      expect(project.valid?).to eq(false)
+    end
+  end
 end
